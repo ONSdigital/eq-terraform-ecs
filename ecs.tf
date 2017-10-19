975947d7d850d97b3a0cb0806c87d683081fbea1
jonnyshaw89 <jon.shaw@jassoft.co.uk>
2017-10-19 16:52:38 +0100

    Lookup ECS ami so we always use the latest one

--- ecs.tf
+++ ecs.tf
@@ -10,9 +10,23 @@ data "template_file" "ecs_user_data" {
   }
 }
 
+data "aws_ami" "amazon_ecs_ami" {
+  most_recent = true
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  name_regex = ".+-amazon-ecs-optimized$"
+
+  most_recent = true
+}
+
+
 resource "aws_launch_configuration" "ecs" {
   name_prefix          = "${var.env}-eq-ecs-"
-  image_id             = "ami-175f1964"                                 // Amazon ECS-Optimized AMI (see: http://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-optimized_AMI.html)
+  image_id             = "${data.aws_ami.amazon_ecs_ami.id}"
   instance_type        = "${var.ecs_instance_type}"
   key_name             = "${var.ecs_aws_key_pair}"
   iam_instance_profile = "${aws_iam_instance_profile.eq_ecs.id}"
