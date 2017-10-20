ac4db80516050a1abd61a3d5804380ef1508cca4
jonnyshaw89 <jon.shaw@jassoft.co.uk>
2017-10-20 12:18:22 +0100

    Added variable for ECS root storage size

--- ecs.tf
+++ ecs.tf
@@ -33,6 +33,11 @@ resource "aws_launch_configuration" "ecs" {
   security_groups      = ["${aws_security_group.eq_ecs_alb_access.id}"]
   user_data            = "${data.template_file.ecs_user_data.rendered}"
 
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = "${var.ecs_instance_storage_size}"
+  }
+
   lifecycle {
     create_before_destroy = true
   }
