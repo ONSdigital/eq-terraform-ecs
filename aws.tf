700978bc47003f095d7644d9f3f2a404f4621a33
Jon Shaw <jon.shaw@jassoft.co.uk>
2018-02-02 14:05:30 +0000

    Fix autoscaling group issue  - Using suggested fix from GitHib Issue

--- aws.tf
+++ aws.tf
@@ -1,7 +1,3 @@
-terraform {
-  required_version = "< 0.10.0" # Depends on https://github.com/terraform-providers/terraform-provider-aws/issues/1376
-}
-
 provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
